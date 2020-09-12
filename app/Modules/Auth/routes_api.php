5163c1d71617162aba05a53e1ecdd8f151367e4f
Yurii Tymchuk <Yurich84@gmail.com>
2020-09-12 19:31:42 +0300

    Name auth routes

--- app/Modules/Auth/routes_api.php
+++ app/Modules/Auth/routes_api.php
@@ -4,18 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
 
-    Route::post('login', 'LoginController@login');
-    Route::post('register', 'RegisterController@register');
+    Route::post('login', 'LoginController@login')->name('login');
+    Route::post('register', 'RegisterController@register')->name('register');
 
-    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
-    Route::post('password/reset', 'ResetPasswordController@reset');
+    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.reset-email');
+    Route::post('password/reset', 'ResetPasswordController@reset')->name('password.reset');
 
     Route::post('email/verify/{user}', 'VerificationController@verify')->name('verification.verify');
-    Route::post('email/resend', 'VerificationController@resend');
+    Route::post('email/resend', 'VerificationController@resend')->name('verification.resend');
 
     Route::group(['middleware' => 'auth:api'], function () {
-        Route::post('logout', 'LoginController@logout');
-        Route::post('me', 'UserController@me');
+        Route::post('logout', 'LoginController@logout')->name('logout');
+        Route::post('me', 'UserController@me')->name('me');
     });
 
 });
