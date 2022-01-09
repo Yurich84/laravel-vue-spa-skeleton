a7985f4262cc4220752132e02e3c64a9e3e1fa75
Yurii Tymchuk <Yurich84@gmail.com>
2022-01-10 00:42:14 +0200

    Remove laravel dusk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-        }
+        //
     }
 
     /**
