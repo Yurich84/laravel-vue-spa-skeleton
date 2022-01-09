6dbfa7f592947d2b62732b22b8a999cdb7eb77c8
Yurii Tymchuk <Yurich84@gmail.com>
2022-01-10 00:11:14 +0200

    Remove Bootstrap and jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,18 +5,9 @@ import i18n from './bootstrap/i18n'
 import router from './bootstrap/router'
 import store from './core/store'
 import globalMixin from './includes/mixins/globalMixin'
-import 'bootstrap'
-import moment from 'moment'
 
 import auth from './bootstrap/auth'
 import './bootstrap/moment'
-
-window.moment = moment
-try {
-    window.$ = window.jQuery = require('jquery')
-} catch (e) {
-    console.log('Error load jquery')
-}
 
 Vue.use(ElementUI, {i18n: (key, value) => i18n.t(key, value)})
 
--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name') }}</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
